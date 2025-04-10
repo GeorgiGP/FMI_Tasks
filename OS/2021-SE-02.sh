8cbb7feacbd6a85ee361ad9ae972295639990c86
GeorgiLazov <georgilazov04@gmail.com>
2025-04-10 14:12:49 -0700

    Update 2021-SE-02.sh

--- OS/2021-SE-02.sh
+++ OS/2021-SE-02.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 if [[ $# -lt 1 ]]; then
-    echo "At least 1 file!"
+    echo "At least 1 file!" 1>&2
     exit 3
 fi
 
 for file in ${@}; do
+    if [[ ! -f "$file" ]]; then
+        echo "$file: Does not exist" 1>&2
+        continue
+    fi
+
     contains=$(cat "$file" | egrep "^.*(([0-9]){10}).*$" | sed -E "s/^.*(([0-9]){10}).*$/\1/")
     if [[ -z "$contains" ]]; then
-        echo "$file: Does not have serial number"
+        echo "$file: Does not have serial number" 1>&2
         continue
     fi
 
@@ -20,7 +25,7 @@ for file in ${@}; do
         id=$(echo $id | sed -E "s/^0([0-9])$/\1/")
         id=$(( $id + 1 ))
         if [[ $id -eq 100 ]]; then
-            echo "$file: Serial date is 99 for today, can't have more changes for today"
+            echo "$file: Serial date is 99 for today, can't have more changes for today" 1>&2
             continue
         fi
         id=$(echo $id | sed -E "s/^([1-9])$/0\1/")
@@ -28,7 +33,6 @@ for file in ${@}; do
         date=$today
         id="00"
     fi
-
 
     sed -E "s/^(.*)$contains(.*)$/\1${date}${id}\2/" -i "$file"
 done
