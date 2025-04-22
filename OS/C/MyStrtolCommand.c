4dcdbc9690978c5f13c01dab09cae0958edee0d7
GeorgiLazov <georgilazov04@gmail.com>
2025-04-22 10:16:58 -0700

    Create MyStrtolCommand.c

---
+++ OS/C/MyStrtolCommand.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include <err.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main(int argc, char** argv) {
+    if ( argc < 2 ) {
+        errx(1, "Missing argument!");
+    }
+
+    char* msg;
+    char* num = argv[1];
+    long res = strtol(num, &msg, 10);
+    if (msg == num) {
+        char ex[128];
+        snprintf(ex, sizeof(ex), "Not a number: %s\n", num);
+        errx(2, ex);
+    }
+    char buf[128];
+    snprintf(buf, sizeof(buf), "%ld\n", res);
+    write(1, buf, strlen(buf));
+
+    if (*msg != '\0') {
+        snprintf(buf, sizeof(buf), "Other args after this: %s\n", msg);
+        write(1, buf, strlen(buf));
+    }
+}
